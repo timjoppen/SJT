2c28c428791654da22d6909118244373179914e5
timjoppen <96329733+timjoppen@users.noreply.github.com>
2023-04-20 08:31:38 +0200

    /
    /

--- python/Som wortels/solution/solution.nl.py
+++ python/Som wortels/solution/solution.nl.py
@@ -1,15 +1,8 @@
-getal = int(input(“Geef een getal”)
-
+getal = int(input("Geef een getal"))
 som = 0
-
 n = 1
-
-While som < getal:
-
-                If (2*n – 1)%3 == 0 or (2*n – 1)%5 == 0
-
-Som += (2*n – 1) **(1/2)*
-
-                n += 1
-
+while som < getal:
+    if (2*n-1)%3 == 0 or (2*n-1)%5 == 0:
+        som += (2*n-1)**(1/2)
+    n += 1
 print(som)
